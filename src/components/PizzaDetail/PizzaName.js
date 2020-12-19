7687c329e5f966e359beda5a4ad0862fab75b53a
buidohuy99 <sunnyhill93@gmail.com>
2020-12-19 10:19:38 +0700

    Initial for Pizza Detail

---
+++ src/components/PizzaDetail/PizzaName.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Divider from '@material-ui/core/Divider';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    parent: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    pizzaName:{
+        fontWeight: 'bold',
+        fontStyle: "italic",
+        marginTop: "1%",
+        marginBottom: "1%"
+    },
+    horizontalLineStyle: {
+        width: "40%",
+        maxWidth: "40%",
+        border: 'none',
+        height: 5,
+        backgroundColor: "black",
+        alignSelf: 'center'
+    }
+}));
+
+const PizzaName = ({pizzaName}) => {
+    const classes = useStyles();
+
+    return(
+        <>
+            <Grid container item xs={12} justify='center' className={classes.parent}>
+                <Typography color='textPrimary' align='center' variant="h5" className={classes.pizzaName}>
+                        {pizzaName? pizzaName : 'Tên bánh'}
+                </Typography>
+                <Divider className={classes.horizontalLineStyle}/>
+            </Grid>
+        </>
+    );
+}
+
+export default PizzaName;

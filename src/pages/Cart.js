8d7fbee5aa18d9c6888b7a06788e6d4ab01ed4c0
johnnyshi1999 <johnnyshi99@gmail.com>
2020-12-27 23:07:26 +0700

    added visual increment/decrement to count text box

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
@@ -34,24 +34,61 @@ const useStyles = makeStyles((theme) => ({
 
 const StyledSum = withStyles(() => ({
     root: {
-    fontStyle: 'normal',
-    fontWeight: 'bold',
-    fontSize: 25,
-    // lineHeight: 42,
-    color: '#000000',
-  },
-  })) (Typography);
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: 25,
+        // lineHeight: 42,
+        color: '#000000',
+    },
+}))(Typography);
+
+function createData(name, size, crust, price, count) {
+    return { name, size, crust, price, count };
+}
 
 const Cart = () => {
     const classes = useStyles();
+    const [sum, setSum] = useState(0);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        const rows = [
+            createData('Pizza', "Lớn", "Dày", 150000, 10),
+            createData('Pizza Đặc Biệt', "Lớn", "Dày", 150000, 3),
+        ];
+        setData(rows);
+        calculateSum(rows);
+
+
+    },[]);
+
+    const calculateSum = (cartData) => {
+        let newSum = 0;
+        cartData.map((row) => {
+            newSum += row.price * row.count;
+        });
+
+        setSum(newSum);
+    };
+
+    const setCount = (index, amount) => {
+        const newData = data.slice();
+        if (newData[index].count + amount > 0) {
+            newData[index].count += amount;
+            setData(newData);
+            calculateSum(data);
+        }
+
+    }
+
 
     return (
-        <Container style={{paddingTop: 50}}>
-            <CartTable></CartTable>
+        <Container style={{ paddingTop: 50 }}>
+            <CartTable cartData={data} setCount={setCount}></CartTable>
             <div className={classes.summary}>
-                <StyledSum style={{marginRight: 10}}>Tổng tiền: </StyledSum>
-                <StyledSum>150000</StyledSum>
-                <ButtonBase style={{marginLeft: 30}}>
+                <StyledSum style={{ marginRight: 10 }}>Tổng tiền: </StyledSum>
+                <StyledSum>{sum}</StyledSum>
+                <ButtonBase style={{ marginLeft: 30 }}>
                     <div style={{ borderRadius: 20, paddingVertical: 15, paddingInline: 40, backgroundColor: '#D2112D' }}>
                         <p style={{ fontWeight: 'bold', color: 'white', fontSize: 15 }}>Thanh Toán</p>
                     </div>
